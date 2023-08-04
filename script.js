ddca73438c88e192858b5ea5aed375d5a1d53e9f
J3wcy <126113811+J3wcy@users.noreply.github.com>
2023-08-03 19:56:28 -0600

    Add function that allows 5 round match with console
    Will declare overall win, loss, or tie as a result of the 5 rounds

--- script.js
+++ script.js
@@ -1,28 +1,51 @@
 const choices = ["Rock", "Paper", "Scissors"];
 
 function getComputerChoice() {
-    const randomIndex = Math.floor(Math.random() * choices.length);
-    const randomChoice = choices[randomIndex];
-    return randomChoice;
+	const randomIndex = Math.floor(Math.random() * choices.length);
+	const randomChoice = choices[randomIndex];
+	return randomChoice;
 }
 
-const userInput = prompt("Enter your choice");
-const playerSelection = userInput ? userInput.toLowerCase() : "";
-const computerSelection = getComputerChoice().toLowerCase();
-
-function playRound(playerSelection, computerSelection){
-    
-    if (playerSelection === "Rock" && computerSelection === "Scissors") {
-        return "You win! Rock beats scissors";
-    } else if (playerSelection === "Scissors" && computerSelection === "Paper") {
-        return "You win! Scissors beats Paper";
-    } else if (playerSelection === "Paper" && computerSelection === "Rock") {
-        return "You win! Paper beats Rock";
-    } else if (playerSelection === computerSelection){
-        return "It's A Tie!";
-    } else {
-        return "You lose!";
-    }
+function playRound(playerSelection, computerSelection) {
+	if (playerSelection === "rock" && computerSelection === "scissors") {
+		return "You win! Rock beats scissors";
+	} else if (playerSelection === "scissors" && computerSelection === "paper") {
+		return "You win! Scissors beats Paper";
+	} else if (playerSelection === "paper" && computerSelection === "rock") {
+		return "You win! Paper beats Rock";
+	} else if (playerSelection === computerSelection) {
+		return "It's A Tie!";
+	} else {
+		return "You lose!";
+	}
 }
 
-console.log(playRound(playerSelection, computerSelection));
+function game() {
+	let playerWins = 0;
+	let computerWins = 0;
+
+	for (let i = 0; i < 5; i++) {
+		const userInput = prompt("Enter your choice");
+		const playerSelection = userInput ? userInput.toLowerCase() : "";
+		const computerSelection = getComputerChoice().toLowerCase();
+		const result = playRound(playerSelection, computerSelection);
+
+		console.log(result);
+
+		if (result.startsWith("You win!")) {
+			playerWins++;
+		} else if (result.startsWith("You lose!")) {
+			computerWins++;
+		}
+	}
+
+	if (playerWins > computerWins) {
+		console.log("Congratulations, You win the majority!");
+	} else if (playerWins < computerWins) {
+		console.log("Sorry, you lose the majority!");
+	} else {
+		console.log("It's a tie in the majority!");
+	}
+}
+game();
+
